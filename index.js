95819f1de502f9de61e2f28240f93d1d22208ae5
Tony Turetsky <TTuretsky@gmail.com>
2018-08-22 14:50:34 -0400

    done

--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const addMembersTrigger = document.getElementById('add-members-trigger')
 
 function addRemaningRadioheadMembers() {
 
-}
+  radioheadRemainingMembers.forEach( member => {
+
+  let objData = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: member, band_id: 2})
+  }
+
+
+  fetch( "http://localhost:3000/members", objData ).then( res => res.json() ).then( data => console.log(data.id) )
+
+  }
+
+)};
+
+
+//redundant code, misunderstood directions below. please ignore:
+
+
+//get fetch, displays ID of all band members
+//
+// function fetchMemberId(){
+//   fetch("http://localhost:3000/members")
+//     .then(res => res.json())
+//     .then( member => member.forEach ( bandMember => console.log(bandMember.id) ) )
+// }
+
 
 addMembersTrigger.addEventListener('click', addRemaningRadioheadMembers)
